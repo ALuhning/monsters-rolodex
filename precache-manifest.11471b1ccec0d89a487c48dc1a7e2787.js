48a0d4e620ed727ac701b9805593d0b06ee0863f
Aaron Luhning <a.luhning@vitalpoint.ai>
2019-11-14 19:16:21 -0500

    Updates

---
+++ precache-manifest.11471b1ccec0d89a487c48dc1a7e2787.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "99ff1b29ddec21dfe8d0b933986056b4",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "df1ee4f1b6f171a30a26",
+    "url": "/monsters-rolodex/static/css/main.7b3bbda3.chunk.css"
+  },
+  {
+    "revision": "c6214f979b742b0bcadf",
+    "url": "/monsters-rolodex/static/js/2.f65d83a6.chunk.js"
+  },
+  {
+    "revision": "df1ee4f1b6f171a30a26",
+    "url": "/monsters-rolodex/static/js/main.079ee995.chunk.js"
+  },
+  {
+    "revision": "ec765bb5ff3b7357d49a",
+    "url": "/monsters-rolodex/static/js/runtime-main.db675a29.js"
+  }
+]);
